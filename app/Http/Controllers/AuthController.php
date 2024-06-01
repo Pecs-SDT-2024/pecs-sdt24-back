ab68995e6b217a0b8227a0970eccca747715fb0a
keruch <verendeev.kirill.2000@gmail.com>
2024-06-01 16:11:07 +0200

    feat: jwt auth draft

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DataResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\ErrorResponse;
+use App\Models\JwtResponse;
+use App\Models\SuccessResponse;
+use App\Models\UserResponse;
+use App\Models\UserRoleMapping;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login_post', 'register']]);
+    }
+
+    public function login_post(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('email', 'password');
+
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json(new ErrorResponse('Unauthorized'), 401);
+        }
+
+        // Encode roles into the JWT token.
+        $user = Auth::user();
+        $roles = [];
+        foreach ($user->roles as $roleMapping) {
+            $roles[] = $roleMapping->role_id;
+        }
+        $claimToken = Auth::claims(['roles' => $roles])->attempt($credentials);
+
+        $responseData = new JwtResponse($claimToken, $this->getExpiration());
+
+        return response()->json(new DataResponse($responseData));
+    }
+
+    public function login_get()
+    {
+        $user = Auth::user();
+        $responseData = new UserResponse($user);
+
+        return response()->json(new DataResponse($responseData));
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        DB::transaction(function() use($request) {
+            /**
+             * @var User
+             */
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            UserRoleMapping::create([
+                'user_id' => $user->id,
+                'role_id' => 1
+            ]);
+        });
+
+        return response()->json(new SuccessResponse());
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json(new SuccessResponse());
+    }
+
+    public function refresh()
+    {
+        $responseData = new JwtResponse(Auth::refresh(), $this->getExpiration());
+        return response()->json(new DataResponse($responseData));
+    }
+
+    /**
+     * Returns the expiration time of the current token in seconds
+     * @return int Expiration time
+     */
+    private function getExpiration() {
+        return Auth::factory()->getTTL() * 60;
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,46 +2,57 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
+
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'hash',
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * The attributes that should be hidden for arrays.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'hash',
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * Get the identifier that will be stored in the subject claim of the JWT.
      *
-     * @return array<string, string>
+     * @return mixed
      */
-    protected function casts(): array
+    public function getJWTIdentifier()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
