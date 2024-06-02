add13301f9a01d3844379527905950cd9d4102fa
keruch <verendeev.kirill.2000@gmail.com>
2024-06-02 19:45:09 +0200

    feat: deleted v1

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 // Post API routes
-Route::get('v1/posts', [PostController::class, 'index']);
-Route::get('v1/posts/{id}', [PostController::class, 'show']);
-Route::post('v1/posts', [PostController::class, 'store']);
-Route::delete('v1/posts/{id}', [PostController::class, 'destroy']);
+Route::get('posts', [PostController::class, 'index']);
+Route::get('posts/{id}', [PostController::class, 'show']);
+Route::post('posts', [PostController::class, 'store']);
+Route::delete('posts/{id}', [PostController::class, 'destroy']);
 
-Route::get('v1/login', [AuthController::class, 'login_get']);
-Route::post('v1/login', [AuthController::class, 'login_post']);
-Route::get('v1/logout', [AuthController::class, 'logout']);
-Route::get('v1/refresh', [AuthController::class, 'refresh']);
-Route::post('v1/register', [AuthController::class, 'register']);
-Route::get('v1/me', [AuthController::class, 'me']);
+Route::get('login', [AuthController::class, 'login_get']);
+Route::post('login', [AuthController::class, 'login_post']);
+Route::get('logout', [AuthController::class, 'logout']);
+Route::get('refresh', [AuthController::class, 'refresh']);
+Route::post('register', [AuthController::class, 'register']);
+Route::get('me', [AuthController::class, 'me']);
